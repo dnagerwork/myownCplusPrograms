9d51126dc750a64d3bb3d38444da4782ac30a5e8
Daniel Nager Carpio <danielnagercarpios@gmail.com>
2024-04-13 02:19:04 +0200

    Adding the description of the calculator to know what will do

---
+++ calculator/main.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    /* My intention is to clone the functionality of calculator CASIO fx-82MS
+     * Shift, Alpha, (left, up, right, down), mode, on
+     * x-1/x!, nCr/nPr, Pol(/Rec( :, x3/square3
+     * Ab/c/d/c, square, x2, log/10x, ln/ex e
+     * (-)/A, .'''/<- B, hyp/C, sin/sin-1 D, cos/cos-1 E, tan-1/F
+     * RCL/STO, END/<-, (, )/X, ,/; Y, M+/M- M (dt cl)
+     * 7, 8, 9, DEL/INS, AC/OFF
+     * 4, 5, 6, X, /
+     * 1/s-sum, 2/s-var, 3, +, -
+     * 0/Rnd, ./Ran#, exp/PI, Ans/DRG, =/%
+     * */ 
+    return 0;
+}
